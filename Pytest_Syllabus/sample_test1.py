75762fa9125990eda994f464eb5f50fdfd5aa621
srtechnologiessravindra <srtechnologiessravindra@gmail.com>
2025-04-26 23:18:10 +0530

    'This is the first commit'

---
+++ Pytest_Syllabus/sample_test1.py
@@ -0,0 +1,15 @@
+#modules should starts with test_
+#functions also should starts with test_
+
+#operators
+def test_equality():
+    assert 5 == 5 #assert can not be used multiple times in a single test case
+
+def test_not_eauality():
+    assert 3 != 5
+
+def test_greater():
+    assert 3 > 5
+
+def test_lesser():
+    assert 3 < 5
